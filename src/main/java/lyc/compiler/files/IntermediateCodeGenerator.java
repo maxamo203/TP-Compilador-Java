8e2206e345620142c5b4c96816fb546df6926c30
maxamo203 <maxamo.203@gmail.com>
2024-10-09 21:27:22 -0300

    identificador secuencial

--- src/main/java/lyc/compiler/files/IntermediateCodeGenerator.java
+++ src/main/java/lyc/compiler/files/IntermediateCodeGenerator.java
@@ -2,21 +2,20 @@ package lyc.compiler.files;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.UUID;
 import guru.nidi.graphviz.engine.Graphviz;
 import guru.nidi.graphviz.model.*;
 import static guru.nidi.graphviz.model.Factory.*;
 import guru.nidi.graphviz.engine.Format;
 
 public class IntermediateCodeGenerator implements FileGenerator {
-
+    private static int tempCount = 0;
     public static MutableGraph graficarNodo(Nodo nodo) {
         if (nodo == null) {
             return mutGraph();
         }
 
         // Creamos un nuevo nodo cada vez, sin importar si su contenido es igual
-        MutableNode nodoActual = mutNode(nodo.getPayload()+"_"+UUID.randomUUID().toString().substring(0, 4));
+        MutableNode nodoActual = mutNode(tempCount++ +"_"+ nodo.getPayload());
 
         // Graficamos recursivamente los nodos hijos
         if (nodo.getLeft() != null) {
@@ -32,8 +31,6 @@ public class IntermediateCodeGenerator implements FileGenerator {
     @Override
     public void generate(FileWriter fileWriter) throws IOException {
         fileWriter.write("TODO");
-        Nodo raiz = Nodo.crearNodo("=", Nodo.crearHijo("a"), Nodo.crearHijo("a"));
-        
         Graphviz.fromGraph(graficarNodo(Punteros.p_triangulo)).width(7000).render(Format.PNG).toFile(new java.io.File("target/output/intermediate-code.png"));
     }
 }
